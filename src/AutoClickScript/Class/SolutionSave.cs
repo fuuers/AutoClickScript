aad8394c0b12d85ed23df15867ecd10dac91d886
xianyuan feng <956749460@qq.com>
2020-05-15 17:28:55 +0800

    class add

---
+++ src/AutoClickScript/Class/SolutionSave.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoClickScript.Class
+{
+    /// <summary>
+    /// 保存和加载
+    /// </summary>
+    class SolutionSave
+    {
+    }
+}
